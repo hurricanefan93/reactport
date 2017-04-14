d0f6b1fc51e0df70acb2efbfb120c1bf1a0b841e
Garret Morales <hurricanefan93@gmail.com>
2017-04-14 16:08:06 -0400

    halfway there

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import ghImage from '../images/githublink.png'
 import fbImage from '../images/facebook-icon.png'
 import twImage from '../images/circle.png'
 import lnkImage from '../images/linkedin-round.png'
+import Repos from './Repos'
 class App extends Component {
 
 state = {
@@ -11,22 +12,16 @@ state = {
   name: [],
   location: [],
   bio: [],
-  repos: [],
-  repoTitle: []
+  starred: [],
+  repoName: []
 }
 
 componentDidMount() {
-    // the URL to "get" todo items
     const user= 'https://api.github.com/users/hurricanefan93'
-    const starred='https://api.github.com/users/hurricanefan93/starred'
-    // make an AJAX request to that URL
+    const starredUrl='https://api.github.com/users/hurricanefan93/starred'
     window.fetch(user)
-      // fetch returns a promsise, which yeilds the "response", we call it 'r'
-      // The response has a method json(), that returns another promise
       .then(r => r.json())
-      // then JSON is done parsing, the promise will yeild the "data"
       .then(data => {
-        // use the data as the state for our items
         this.setState({
           profile: data.avatar_url,
           name: data.name,
@@ -34,17 +29,14 @@ componentDidMount() {
           bio: data.bio
         })
       })
-      window.fetch(starred)
+      window.fetch(starredUrl)
       .then(r => r.json())
       .then(data => {
-        for(let i = 0; i < data.length; i++) {
           this.setState({
-            repos: [...this.state.repos, data[i].html_url],
-            repoTitle: [...this.state.repoTitle, data[i].description]
+            starred: data
           })
-        }
       })
-  }
+    }
 
 
   render () {
@@ -52,12 +44,12 @@ componentDidMount() {
     <header>
     <div><h1>{this.state.name}</h1></div>
     <div><img className='headshot' src={this.state.profile} /></div>
-    <div><p>{this.state.location}</p></div>
+    <div className='location'><p>{this.state.location}</p></div>
     <div className='bio'><p>{this.state.bio}</p></div>
-    <a href={this.state.repos[0]}>{this.state.repoTitle[0]} </a>
-    <a href={this.state.repos[1]}>{this.state.repoTitle[1]} </a>
-    <a href={this.state.repos[2]}>{this.state.repoTitle[2]} </a>
     </header>
+    <div className='workinfo'>
+    <Repos starred={this.state.starred} />
+    </div>
     <footer>
     <a href='https://github.com/hurricanefan93'><img src={ghImage} height='75' width='75' /></a>
     <a href='https://www.facebook.com/garret.morales.5'><img src={fbImage} height='75' width='75' /></a>
---
+++ src/components/Repos.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Repos extends Component {
+
+  render () {
+    const link = this.props.starred.map((repo, i) => {
+      return <a className={`pic${i + 1}`} href={repo.html_url} key={i}>
+        <img src={`https://raw.githubusercontent.com/hurricanefan93/${repo.name}/master/screenshot.png`} height='120' width='180' key={i} />
+      </a>
+    })
+
+    return (
+      <div className='linkpics'>
+        {link}
+      </div>
+    )
+  }
+}
+
+export default Repos
